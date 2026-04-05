98c8a1f7e0c3c492468b9f76879781ed92840394
Petro <petro@petro.ws>
2026-04-05 20:42:03 +0200

    Revert wrong changes for xsns_17_senseair.ino

--- tasmota/tasmota_xsns_sensor/xsns_17_senseair.ino
+++ tasmota/tasmota_xsns_sensor/xsns_17_senseair.ino
@@ -281,9 +281,6 @@ bool Xsns17(uint32_t function)
 #endif  // USE_WEBSERVER
       SenseairShow(function);
       break;
-    case FUNC_EVERY_SECOND:
-      GalopedLoop();
-      break;
   }
   return false;
 }
